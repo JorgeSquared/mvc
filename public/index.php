d153b3e5991e6e583006ad7ae8ad599a85d7ffb6
jirka <jorgetimes2@gmail.com>
2021-03-18 14:35:06 +0100

    index.php file with route paths

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * From now on, this file will serve as our front controller.
+ * It is a place, where every URL is mapped to trigger a specific predefined action,
+ * not like the other way round, where in the spaghetti-like projects, every single
+ * URL maps to a specific *.php file, rendering the code unmaintainable
+ */
+
+
+echo '"Requested URL = "' . $_SERVER['QUERY_STRING'];
