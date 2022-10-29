b3b8e212f86d723a5f7aa9b5c8fc235eb92171a9
jirka <jorgetimes2@gmail.com>
2022-10-29 12:15:35 +0200

    add custom variables of any format to the url

--- Core/Router.php
+++ Core/Router.php
@@ -31,6 +31,9 @@ class Router
         // Convert variable parts, e.g. {controller} of the route to a named part of the regular expression
         $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
 
+        // Convert variables with custom regular expressions e.g. {id:\d+}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
         // Add start and end delimiters, and case insensitive flag
         $route = '/^' . $route . '$/i';
 
